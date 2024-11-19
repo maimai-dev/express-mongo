84bec802856f918404c4341d9244c2f48824838a
Maiara Barbosa Nunes <maiaranunes.dev@gmail.com>
2024-11-19 18:52:08 -0300

    commit 19/11

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -59,7 +59,7 @@ class LivroController {
     static async listarLivrosPorEditora (req, res){
         const editora = req.query.editora;
         try{
-            const livrosPorEditora = await livro.find({}).populate('editora');
+            const livrosPorEditora = await livro.find({'editora.nome': editora}).populate('editora');
             res.status(200).json(livrosPorEditora)
         } catch (erro){
             res.status(500).json({message: `${erro.message} - falha na busca`})
